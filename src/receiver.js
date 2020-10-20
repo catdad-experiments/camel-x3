2ab42684a8256bb5fed38028e0e26f49f9910efa
Kiril Vatev <vatev.1@gmail.com>
2020-10-20 19:40:34 -0400

    handling actual shared data and showing buttons to open in Chrome/Firefox Focus

--- src/receiver.js
+++ src/receiver.js
@@ -8,14 +8,27 @@ import { html, render } from './preact.js';
 </div>
 */
 
+const getCamelUrl = productUrl => {
+  return `camelcamelcamel.com/search?sq=${encodeURIComponent(productUrl)}`;
+};
+
 export default ({ events }) => {
   const elem = document.querySelector('#main');
+  const regex = /(https?:\/\/(?:[^.]+\.)?amazon\.[^ ]+)/;
 
   const splash = ({ title, text, url }) => {
+    const [, textUrl] = text.match(regex) || [];
+    const camelUrl = getCamelUrl(url || textUrl);
+
     const stuff = html`<div class=limit>
       <div>title: ${title}</div>
       <div>text: ${text}</div>
       <div>url: ${url}</div>
+      <div>camel url: ${camelUrl}</div>
+      <div>
+        <div><a href="intent://${camelUrl}#Intent;scheme=https;package=org.mozilla.focus;end;">Open in Firefox Focus</a></div>
+        <div><a href="intent://${camelUrl}#Intent;scheme=https;package=com.android.chrome;end;">Open in Chrome</a></div>
+      </div>
     </div>`;
 
     render(stuff, elem);
