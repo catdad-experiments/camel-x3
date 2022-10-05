ab0225e809feb79bae40981263e1cfca68195f31
Kiril Vatev <vatev.1@gmail.com>
2022-10-05 18:57:58 -0400

    switching to using htm's preact build, since it's easier and has all the exports in one place

--- src/preact.js
+++ src/preact.js
@@ -1,5 +1,33 @@
-import { h, render } from 'https://cdn.jsdelivr.net/npm/preact@10.4.0/dist/preact.module.js';
-import htm from 'https://cdn.jsdelivr.net/npm/htm@3.0.4/dist/htm.module.js';
-const html = htm.bind(h);
+import {
+  html,
+  render,
+  createContext,
+  useState,
+  useReducer,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useImperativeHandle,
+  useMemo,
+  useCallback,
+  useContext,
+  useDebugValue,
+  useErrorBoundary
+} from 'https://cdn.jsdelivr.net/npm/htm@3.1.1/preact/standalone.module.js';
 
-export { html, render };
+export {
+  html,
+  render,
+  createContext,
+  useState,
+  useReducer,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useImperativeHandle,
+  useMemo,
+  useCallback,
+  useContext,
+  useDebugValue,
+  useErrorBoundary
+};
